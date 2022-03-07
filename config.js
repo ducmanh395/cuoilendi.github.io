691ee4c889896060dd8d6ea9c37b4bf656d845d7
ducmanh395 <86871386+ducmanh395@users.noreply.github.com>
2022-03-07 19:54:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ const CONFIG = {
     btnNo: 'cậu mơ à :333',
     question:'Nói cho tới lý do vì sao cậu yêu tớ đii <3',
     btnReply: 'Gửi cho tớ <3',
-    reply: 'Tại vì cậu đẹp trai hihi <33333333',
-    mess: 'Tớ biết mà 🥰. Love you 300.000 😘😘',
+    reply: 'Tình yêu không cần nói nhiều 😏',
+    mess: 'Tớ biết mà 🥰. Nhưng mà xin lỗi tớ thích bạn khác mất rồi hì hì 😅',
     messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100011536002779' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
